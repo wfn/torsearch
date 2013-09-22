c3915494d48d719352e2a5507566d7c94efb7fe0
Kostas Jakeliunas <kostas@jakeliunas.com>
2013-09-22 21:09:26 +0000

    Add explicit threaded=true for werkzeug's engine
    When using the internal http engine, make sure Werkzeug gets threaded=true
    passed to it by Flask.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from torsearch import app
 import config
 
-app.run(debug = config.DEBUG, host=config.BIND_HOST, port=config.BIND_PORT)
+app.run(debug = config.DEBUG, host=config.BIND_HOST, port=config.BIND_PORT, threaded=True)
