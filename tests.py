19aa556e4c20fd539d2d040f66a2bb3ded74efd1
Kostas Jakeliunas <kostas@jakeliunas.com>
2013-10-10 23:31:19 +0000

    Add a test
    test_known_relays_in_results(): search for relays, check if all good,
    and store results in a list accessible from other test_*() cases for
    later use (for get_statuses() checks, etc.)

--- tests.py
+++ tests.py
@@ -8,13 +8,23 @@ from torsearch import onionoo_api as oapi
 
 class TestOnionooAPISearch(unittest.TestCase):
   def setUp(self):
-    pass
+    self.fingerprints = []
 
   def test_default_search(self):
     consensus, results = oapi.get_results(args={'bogus': True})
     results = list(results) # iterator=>list
     self.assertEqual(len(results), oapi.UPPER_LIMIT)
     self.assertEqual(results[0].last_va, consensus.valid_after)
+
+  def test_known_relays_in_results(self):
+    known_relay_nicks = ['moria1', 'faravahar']
+    for nick in known_relay_nicks:
+      consensus, results = oapi.get_results(args={'search': nick})
+      last_count = len(self.fingerprints)
+      for r in results:
+        if r.last_va == consensus.valid_after:
+          self.fingerprints.append(r)
+      self.assertTrue(last_count < len(self.fingerprints))
 
 if __name__ == '__main__':
   unittest.main()
